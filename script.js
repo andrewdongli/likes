7cb5225f73eaea15157d359f1a77fea4d4c05c2b
Andrew Li <liandrewdong@gmail.com>
2021-08-24 14:16:25 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var count1=9;
+function countUp1(){
+    count1++
+    myEle = document.querySelector(".first-like");
+    console.log(myEle);
+    myEle.innerHTML = count1;
+}
+
+var count2=12;
+function countUp2(){
+    count2++
+    myEle = document.querySelector(".second-like");
+    console.log(myEle);
+    myEle.innerHTML = count2;
+}
+
+var count3=9;
+function countUp3(){
+    count3++
+    myEle = document.querySelector(".third-like");
+    console.log(myEle);
+    myEle.innerHTML = count3;
+}
